dafed09af486cd5f99663f44dad0467ecce89023
Ramiro Garces <ramgarces24@gmail.com>
2023-03-04 23:36:36 -0600

    Fetch individual character

--- src/views/CharacterDetails.vue
+++ src/views/CharacterDetails.vue
@@ -1,13 +1,27 @@
 <script setup>
 import { useRoute } from 'vue-router'
+import { onMounted } from 'vue'
+
+import useCharacters from '@/composables/useCharacters'
 
 const route = useRoute()
+const { fetchCharacter, currentCharacter } = useCharacters()
+
+onMounted(async () => {
+  await fetchCharacter(route.params.id)
+})
+
+
 </script>
 
 <template>
-  <main class="flex min-h-screen items-center justify-center">
-    <h1 class="text-6xl font-thin text-slate-800">
-      Hi, I'm Character {{  route.params.id }}
+  <main 
+    v-if="currentCharacter" 
+    class="flex flex-col gap-6 min-h-screen items-center justify-center bg-gradient-to-b from-black to-purple-900">
+    <img :src="currentCharacter.imageUrl" :alt="currentCharacter.name" />
+    <h1 class="text-6xl font-bolder text-white">
+      Hi, I'm {{ currentCharacter.name }}
     </h1>
+    <pre class="text-white">{{ currentCharacter }}</pre>
   </main>
 </template>
